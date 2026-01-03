544ba38cf8cecbe48b9ca977a9b3fe5447d442e0
sbmswebby <sbmswebby@gmail.com>
2026-01-03 13:48:43 +0530

    No change made

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,9 @@ import HeroText from "@/components/Hero/HeroText";
 export default function Home() {
   return (
     <div className="min-h-screen w-full flex flex-col">
+      
       {/* Hero Section */}
+      <>
       <GridWithGlow>
         {/* Center content with flexbox */}
         <div className="flex flex-col w-full items-center justify-center min-h-screen gap-10 translate-z-0">
@@ -12,6 +14,8 @@ export default function Home() {
           <ExplosionButton />
         </div>
       </GridWithGlow>
+      </>
+
     </div>
   );
 }
